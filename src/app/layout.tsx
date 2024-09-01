ca68fc9250beedd0b75f1a7d1b8b683000173637
Mateo <rayoman01@gmail.com>
2024-09-01 15:04:58 -0400

    Convex parte 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
       <body className={inter.className}>
 
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-	{children}
-</ThemeProvider>
+        <ConvexClientProvider>
+        {children}
+        </ConvexClientProvider>
+      </ThemeProvider>
       </body>
     </html>
   );
